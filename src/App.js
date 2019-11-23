f680a8b40da6bc9214d034207a36b8fa24c5a51b
Danshin <si1ver300594@gmail.com>
2019-11-23 11:33:50 +0600

    #6 add ListMenu Const in app.js

--- src/App.js
+++ src/App.js
@@ -3,10 +3,19 @@ import './App.css';
 import MenuList from './components/MenuList/MenuList.jsx'
 import OrderDetail from './components/OrderDetails/OrderDetails.jsx'
 
+const menu = [
+  {name : 'Hamburger', price:80 , icon : 'image'},
+  {name : 'Cheeseburger', price:90 , icon : 'image'},
+  {name : 'Fries', price:45 , icon : 'image'},
+  {name : 'Coffee', price:70 , icon : 'image'},
+  {name : 'Tea', price:50 , icon : 'image'},
+  {name : 'Cola', price:40 , icon : 'image'},
+]
+
 class App extends Component {
  render(){
   return (
-    <div className="App">
+    <div className="App d-flex w-75 border border-primary">
       <OrderDetail />
       <MenuList />
     </div>
